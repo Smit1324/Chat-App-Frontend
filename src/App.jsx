a973862487956951ad1a739c50a24b5af2a38bd5
Smit <smitmpatil123@gmail.com>
2024-02-21 10:49:18 +0530

    Routing & Router Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import Home from "./pages/Home"
+import Signup from "./pages/Signup"
+import Signin from "./pages/Signin"
+import Profile from "./pages/Profile"
+import UpdateProfile from "./pages/UpdateProfile"
 
 const App = () => {
 
     return (
-        <div>App</div>
+
+        <Routes>
+            <Route path="/" element={<Signup />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/update-profile" element={<UpdateProfile />} />
+        </Routes>
+
     )
 }
 
